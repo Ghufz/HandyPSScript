6afd8c92d0f76f67aa164b87fb09537bc4ce35e1
Ghufran Zahidi <gzahidi@cisco.com>
2020-04-17 11:06:44 +0530

    Sync fork repo script

---
+++ SyncTheForkRepoTpUpstream.ps1
@@ -0,0 +1,22 @@
+﻿param(
+[Parameter(Mandatory=$false)]
+[string] $RepoPath = $PSScriptRoot 
+)
+
+Write-host "Sync the fork repo"
+Set-Location -Path $RepoPath
+
+Write-host "Get the Remote "
+git remote -v
+
+Write-host "Pull the upstream"
+git fetch upstream
+
+Write-host "Check out master"
+git checkout master
+
+Write-host "Merge the upstream/master to master"
+git merge upstream/master
+
+Write-Host "Push to the master"
+git push
