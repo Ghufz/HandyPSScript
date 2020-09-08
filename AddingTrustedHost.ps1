04a8b14b77cc298f0fec3803c93d565c899c144e
Ghufran Zahidi <gzahidi@cisco.com>
2020-09-08 12:02:36 +0530

    Test.ps1 to test action

---
+++ AddingTrustedHost.ps1
@@ -0,0 +1,25 @@
+﻿$ERCSIPAddress = "192.168.20.27"
+$isERCSIPAdded = $false
+    #Check for TrustedHosts
+    $trustedHost = (Get-Item WSMan:\localhost\Client\TrustedHosts).Value
+    if($trustedHost -ne "*")
+    {
+        if($trustedHost -match ","){
+            if($trustedHost.Split(',') -contains $ERCSIPAddress){
+                $isERCSIPAdded = $true
+                Write-host "$ERCSIPAddress is already added to trusted host"
+            }
+        }elseif($trustedHost -eq $ERCSIPAddress){
+            $isERCSIPAdded = $true
+            Write-host "$ERCSIPAddress is already added to trusted host"
+        }
+
+        if(!$isERCSIPAdded){
+        Write-host "Adding ERCSIPAddress $ERCSIPAddress as trusted client to establish remote session."
+        Set-Item WSMan:\localhost\Client\TrustedHosts -Value $ERCSIPAddress -Concatenate -Force
+        }
+    }
+    else
+    {
+        Write-host "Trusted host is already set to '*'"
+    }
