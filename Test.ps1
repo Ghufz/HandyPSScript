dfa2eaa613ec82f91b571dd9b85a14e83d67db5b
Ghufran Zahidi <gzahidi@cisco.com>
2020-09-08 12:23:29 +0530

    Action file updated for Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,4 @@
 Write-Host "I am from file"
 New-Item -Name Build -ItemType Directory
-New-Item -Name .\Build\Test.txt -ItemType File
+New-Item -Name Build\PowerShell -ItemType Directory
+New-Item -Name .\Build\PowerShell\Test.txt -ItemType File
